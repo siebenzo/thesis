e5e5dc1f487f5ee955ee0309b22a57854705e449
QuintenMartens <115155172+QuintenMartens@users.noreply.github.com>
2023-05-08 00:15:23 +0200

    Files waarmee ML op computer WERKT

---
+++ 4_Video_Feed_werkende.ino
@@ -0,0 +1,78 @@
+#include "esp32cam.h"
+#include "esp32cam/http/LiveFeed.h"
+#include "eloquent.h"
+#include <WiFi.h>;
+
+// Replace with your WiFi credentials
+#define WIFI_SSID "www.huurinleuven.beT23"
+#define WIFI_PASS "ineedinternet"
+#define CAMERA_MODEL_WROVER_KIT
+#include "eloquent/vision/camera/wrover.h"
+
+
+// 80 is the port to listen to
+// You can change it to whatever you want, 80 is the default for HTTP
+Eloquent::Esp32cam::Cam cam;
+Eloquent::Esp32cam::Http::LiveFeed feed(cam, 80);
+WiFiServer server(81);
+WiFiClient client;
+
+void setup() {
+    Serial.begin(115200);
+    delay(3000);
+    Serial.println("Init");
+
+    // see 3_Get_Your_First_Picture for more details
+    cam.wrover();
+    cam.highQuality();
+    cam.qqvga();
+    
+    
+
+    while (!cam.begin())
+        Serial.println(cam.getErrorMessage());
+
+    // Connect to WiFi
+    // If something goes wrong, print the error message
+    while (!cam.connect(WIFI_SSID, WIFI_PASS))
+        Serial.println(cam.getErrorMessage());
+
+    //Initialize live feed http server
+    // If something goes wrong, print the error message
+    while (!feed.begin())
+        Serial.println(feed.getErrorMessage());
+
+    // make the camera accessible at http://esp32cam.local
+    if (!cam.viewAt("esp32cam"))
+        Serial.println("Cannot create alias, use the IP address");
+    else
+        Serial.println("Live Feed available at http://esp32cam.local");
+
+    // display the IP address of the camera
+    Serial.println(feed.getWelcomeMessage());
+    server.begin();
+    pinMode(15, OUTPUT);
+}
+
+
+void loop() {
+  // Check if a client has connected
+    if (client.connected()) {
+        // Read any data that has been sent
+        while (client.available()) {
+            char c = client.read();
+            Serial.print(c);
+            digitalWrite(15,HIGH);
+        }
+
+        // If the client has disconnected, close the connection
+        if (!client.connected()) {
+            
+            client.stop();
+        }
+    }
+    else {
+        // If no client is connected, wait for a new connection
+        client = server.available();
+    }
+}
